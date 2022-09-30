5b2eed9d91d8128614d4dd686ea68582d9c31d66
Jialiang Cao <jialiangc@nycstudents.net>
2022-09-30 11:27:33 -0400

    Initial commit

---
+++ src/CircleRunner.java
@@ -0,0 +1,16 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circle1 = new Circle(5);
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circle1.printArea();
+        // C. BEFORE running this code, PREDICT what gets printed
+        /* I predict it will print out the area of the circle calculated by multiplying the radius to 3.14 then to the radius again*/
+
+        // D. run the code and confirm your prediction! Were you correct?
+        /* Yes I was Correct */
+    }
+}
+
