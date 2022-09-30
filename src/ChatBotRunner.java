8732ae813672312804ce8ba3a061a35112360e16
Jialiang Cao <jialiangc@nycstudents.net>
2022-09-30 12:30:17 -0400

    Doing Chatbot

---
+++ src/ChatBotRunner.java
@@ -0,0 +1,9 @@
+public class ChatBotRunner {
+    public static void main(String[] args) {
+        ChatBot deb = new ChatBot("Debbie", 13);
+        deb.greeting("Jialiang");
+        deb.weather();
+        System.out.println("Did you know that 10 feet is " );
+
+    }
+}
